5f3f1a074372d0a7c00ca3f9aa2965e1d7629296
Muhammad Hasham <hasham25525@gmail.com>
2023-10-31 02:08:49 +0500

    homepage basic version

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import DemoGif from "../images/landingPage/demo.png"
+import DemoGif from "../images/landingPage/demo.png";
 
 export default function Home() {
   return (
@@ -50,7 +50,7 @@ export default function Home() {
             <div className="mt-16 flow-root sm:mt-24">
               <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <Image
-                unoptimized
+                  unoptimized
                   src={DemoGif}
                   alt="App screenshot"
                   width={2432}
@@ -60,6 +60,20 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </div>
+
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13 rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30 rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3 rem)] aspect-[1155/678] w-[36.125 rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30
+sm:left-[calc(50%+36 rem)] sm:w-[72.1875 rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
         </div>
       </div>
     </main>
