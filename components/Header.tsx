c41f7a40c030ff227712076b955a07e924b07b89
Muhammad Hasham <hasham25525@gmail.com>
2023-10-31 01:52:48 +0500

    home page designing

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,8 +12,8 @@ async function Header() {
   
 
   return (
-    <header className="sticky top-0 z-50 bg-white dark:bg-slate-950">
-      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-slate-900 max-w-7xl mx-auto">
+    <header className="sticky top-0 z-50  bg-white dark:bg-transparent dark:backdrop-brightness-50 dark:backdrop-blur-md">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 max-w-7xl mx-auto ">
         <Logo />
         <div className="flex-1 flex items-center justify-end space-x-4">
           {/* language */}
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <Link href="/" prefetch={false} className="overflow-hidden">
-      <div className="flex items-center w-72 h-14 ">
+      <div className="flex items-center w-72 h-10 ">
         <AspectRatio
           ratio={16 / 9}
           className="flex items-center justify-center"
