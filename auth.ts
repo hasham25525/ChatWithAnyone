1d8f05d0c301c5161feeeb47f2d396a84cc17712
Muhammad Hasham <hasham25525@gmail.com>
2023-11-03 00:59:53 +0500

    checkout button

--- auth.ts
+++ auth.ts
@@ -1,12 +1,14 @@
 import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-           
-          })
-    ],
-}satisfies NextAuthOptions;
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+} satisfies NextAuthOptions;
--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+"use client";
+
+import { useSession } from "next-auth/react";
 
 function CheckoutButton() {
+  const { data: session } = useSession();
+
+  const createCheckoutSession = async () => {
+    if (!session) return;
+
+    //push a doc in firestore
+
+    //... strioe extension on firestore will create a session
+
+    // redirect user to checkout page
+  };
+
   return (
-   <>
-   {/* if sub show mw the user is subed */}
-    <button
-      className="mt-8 block rounded-md bg-indigo-600 hover:bg-indigo-500 
+    <div className="flex flex-col space-y-2">
+      {/* if sub show mw the user is subed */}
+      <button
+        onClick={() => createCheckoutSession()}
+        className="mt-8 block rounded-md bg-indigo-600 hover:bg-indigo-500 
     px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm 
     disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default"
-    >
-      Checkout
-    </button>
-   </>
+      >
+        Checkout
+      </button>
+    </div>
   );
 }
 
