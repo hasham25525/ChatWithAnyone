d18714480dc43e88940451d9db7707566af6aeb0
Muhammad Hasham <hasham25525@gmail.com>
2023-11-04 01:54:47 +0500

    add doc to firebase

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -1,15 +1,28 @@
 "use client";
 
+import { db } from "@/firebase";
+import { addDoc, collection } from "firebase/firestore";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 function CheckoutButton() {
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
 
   const createCheckoutSession = async () => {
-    if (!session) return;
+    if (!session?.user.id) return;
 
     //push a doc in firestore
+    setLoading(true);
 
+    const docRef = await addDoc(
+      collection(db, "customers", session.user.id, "checkout_session"),
+      {
+        price: "price_10099",
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      }
+    );
     //... strioe extension on firestore will create a session
 
     // redirect user to checkout page
